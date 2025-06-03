532911ad264ea40b6d986d52b7c103505b3f5872
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-06-03 16:16:30 -0300

    testing db connection and web console

--- src/main/java/com/utn/appparcial/Main.java
+++ src/main/java/com/utn/appparcial/Main.java
@@ -1,4 +1,19 @@
 package com.utn.appparcial;
 
+import com.utn.appparcial.util.DatabaseUtil;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        try (Connection conn = DatabaseUtil.getConnection()) {
+            Statement stmt = conn.createStatement();
+            DatabaseUtil.startWebConsole();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
