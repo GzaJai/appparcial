d2b955eacc269ec762eb834041d913cb28db259c
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-06-03 20:54:18 -0300

    creating models

---
+++ src/main/java/com/utn/appparcial/model/Professor.java
@@ -0,0 +1,16 @@
+package com.utn.appparcial.model;
+
+public class Professor extends User {
+
+    public Professor() {
+    }
+
+    public Professor(int id, String name, String lastName, String email, String password) {
+        super(id, name, lastName, email, password);
+    }
+
+    // methods
+    public String toString() {
+        return "Profesor{nombre" + this.getName() + ", apellido=" + this.getLastName() + ", email= " + this.getEmail() +"}";
+    }
+}
---
+++ src/main/java/com/utn/appparcial/model/Student.java
@@ -0,0 +1,17 @@
+package com.utn.appparcial.model;
+
+public class Student extends User {
+    private static float averageGrade;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, String lastName, String email, String password) {
+        super(id, name, lastName, email, password);
+    }
+
+    // methods
+    public String toString() {
+        return "Alumno{nombre" + this.getName() + ", apellido=" + this.getLastName() + ", email= " + this.getEmail() +"}";
+    }
+}
