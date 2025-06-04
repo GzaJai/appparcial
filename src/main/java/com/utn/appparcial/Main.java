8a7e0a4637519fec888cb9961ab241162d58ad3e
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-06-04 12:57:33 -0300

    removing unused methods

--- src/main/java/com/utn/appparcial/Main.java
+++ src/main/java/com/utn/appparcial/Main.java
@@ -17,42 +17,12 @@ public class Main {
 
         try (Connection conn = DatabaseUtil.getConnection()) {
             DatabaseUtil.initialize(conn);
-            //initializeAdminProfessor(conn);
-            // createDefaultStudents(conn);
 
             ConsoleApp consoleApp = new ConsoleApp(conn);
             consoleApp.run();
         }
         catch (SQLException e) {
             e.printStackTrace();
-        }
-    }
-
-    private static void initializeAdminProfessor(Connection connection) {
-        ProfessorDAO professorDAO = new ProfessorDAOImpl(connection);
-
-        Professor existing = professorDAO.findByEmailAndPassword("admin@utn.com", "admin123");
-
-        if (existing == null) {
-            Professor defaultProfessor = new Professor(null, "Admin", "UTN", "admin@utn.com", "admin123");
-            professorDAO.save(defaultProfessor);
-            System.out.println("\n\nprofesor guardaro: " + defaultProfessor.getEmail() + " " + defaultProfessor.getPassword());
-            System.out.println(">>> Profesor administrador creado por defecto <<<");
-        }
-    }
-
-    private static void createDefaultStudents(Connection connection) {
-        StudentDAO studentDAO = new StudentDAOImpl(connection);
-
-        List<Student> existing = studentDAO.findAll();
-        if (existing.isEmpty()) {
-            Student s1 = new Student(null, "Juan", "Pérez", "juan.perez@utn.com", "1234");
-            Student s2 = new Student(null, "Lucía", "Gómez", "lucia.gomez@utn.com", "1234");
-            studentDAO.save(s1);
-            studentDAO.save(s2);
-
-            System.out.println("\n>>> Alumnos de prueba creados por defecto <<<\n\n\n\n\n\n\n\n\n");
-
         }
     }
 
