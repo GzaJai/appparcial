04ae84ee22ee803f46cbc3b76afe5f2c9a6db2b8
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-06-03 22:47:41 -0300

    creating DAO generic interface

---
+++ src/main/java/com/utn/appparcial/dao/GenericDAO.java
@@ -0,0 +1,11 @@
+package com.utn.appparcial.dao;
+
+import java.util.List;
+
+public interface GenericDAO<T, ID> {
+    void save(T entity);
+    void update(T entity);
+    void delete(T entity);
+    T findById(ID id);
+    List<T> findAll();
+}
