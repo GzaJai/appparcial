2b160d8ec0610e1846a62cf22b3b7eaae7f7f477
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-06-04 12:57:51 -0300

    removing unused imports

--- src/main/java/com/utn/appparcial/Main.java
+++ src/main/java/com/utn/appparcial/Main.java
@@ -1,16 +1,9 @@
 package com.utn.appparcial;
 
 import com.utn.appparcial.app.ConsoleApp;
-import com.utn.appparcial.dao.ProfessorDAO;
-import com.utn.appparcial.dao.StudentDAO;
-import com.utn.appparcial.dao.impl.ProfessorDAOImpl;
-import com.utn.appparcial.dao.impl.StudentDAOImpl;
-import com.utn.appparcial.model.Professor;
-import com.utn.appparcial.model.Student;
 import com.utn.appparcial.util.DatabaseUtil;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
